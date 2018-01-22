35fe7cfa0f1743bbfb803d73fa6835c026ea8595
Tomasz Łakomy <tlakomy90@gmail.com>
2018-01-23 00:26:06 +0100

    Kinda, sorta works

--- vr/client.js
+++ vr/client.js
@@ -1,17 +1,3 @@
-/**
- * The examples provided by Oculus are for non-commercial testing and
- * evaluation purposes only.
- *
- * Oculus reserves all rights not expressly granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
- * OCULUS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
- * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 import {VRInstance} from 'react-vr-web';
 import {Module} from 'react-vr-web';
 import * as THREE from 'three';
@@ -47,8 +33,11 @@ class RCTTestDom extends ReactVR.RCTBaseView {
     const tagString = `
     <a-scene embedded arjs='trackingMethod: best;'>
         <a-anchor hit-testing-enabled='true'>
-        <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
-        </a-anchor>
+        <a-box position='0 0.5 0' material='opacity: 0.5; side: double; color: blue;'>
+          <a-torus-knot radius='0.26' radius-tubular='0.05'>
+            <a-animation attribute="rotation" to="360 0 0" dur="3000" easing='linear' repeat="indefinite"></a-animation>
+          </a-torus-knot>
+        </a-box>
       <a-camera-static/>
     </a-scene>`;
 
@@ -80,58 +69,19 @@ class RCTTestDom extends ReactVR.RCTBaseView {
 function init(bundle, parent, options) {
   // Create a scene so we can add to it; otherwise the VRInstance creates one.
   const scene = new THREE.Scene();
-  // Create Native Module so React context can modify native (Three.js) cube.
-  const cubeModule = new CubeModule();
 
   const vr = new VRInstance(bundle, 'CubeSample', parent, {
     cursorVisibility: 'visible',
-    nativeModules: [cubeModule],
     customViews: [{name: 'TestDom', view: RCTTestDom}],
     scene: scene,
   });
 
-  // Custom scene setup goes here. Add a cube to the scene.
-  // Must be done after providing the scene to VRInstance above.
-  const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial());
-  cube.position.z = -4;
-  scene.add(cube);
-
-  // Give cubeModule a handle to our cube.
-  cubeModule.init(cube);
-
   vr.render = function(timestamp) {
-    // Custom per-frame updates go here. Bounce the cube up and down.
-    const seconds = timestamp / 1000;
-    cube.position.x = 0 + 1 * Math.cos(seconds);
-    cube.position.y = 0.2 + 1 * Math.abs(Math.sin(seconds));
   };
 
   // Begin the animation loop
   vr.start();
   return vr;
-}
-
-export default class CubeModule extends Module {
-  // CubeModule is a React Native Module, which implements functionality
-  // that can be called asynchronously across the React Native brige.
-
-  // Constructor calls super() with one argument: module name.
-  constructor() {
-    super('CubeModule');
-  }
-
-  // Called directly after the module is created.
-  init(cube) {
-    this.cube = cube;
-  }
-
-  // Change the cube material color to the given value.
-  // Called remotely by the CubeModule on the React side.
-  changeCubeColor(color) {
-    // THREE.Color() accepts either a six-digit hex color or a CSS style.
-    // e.g. 0xff0000, 'rgb(255,0,0)', 'red'
-    this.cube.material.color = new THREE.Color(color);
-  }
 }
 
 window.ReactVR = {init};
