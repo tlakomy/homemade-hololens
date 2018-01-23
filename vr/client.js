967482ca82978f454a21bf47dd9f3158c5577097
Tomasz Łakomy <tlakomy90@gmail.com>
2018-01-23 21:08:29 +0100

    Added local vendors (just in case) and some .ply files

--- vr/client.js
+++ vr/client.js
@@ -32,14 +32,16 @@ class RCTTestDom extends ReactVR.RCTBaseView {
     this.view = new OVRUI.UIView(guiSys);
     const tagString = `
     <a-scene embedded arjs='trackingMethod: best;'>
-        <a-anchor hit-testing-enabled='true'>
+      <a-anchor hit-testing-enabled='true'>
         <a-box position='0 0.5 0' material='opacity: 0.5; side: double; color: blue;'>
           <a-torus-knot radius='0.26' radius-tubular='0.05'>
             <a-animation attribute="rotation" to="360 0 0" dur="3000" easing='linear' repeat="indefinite"></a-animation>
           </a-torus-knot>
         </a-box>
+      </a-anchor>
       <a-camera-static/>
-    </a-scene>`;
+    </a-scene>
+    `;
 
     const range = document.createRange();
     // make the parent of the first div in the document becomes the context node
